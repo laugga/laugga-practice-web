4bbf767c169208f6e892a681fa42d17ce371f1bc
Luis Laugga <luis@laugga.com>
2023-06-06 23:30:31 +0200

    Add seo component using data from cms.

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,50 +1,133 @@
-/**
- * SEO component that queries for data with
- * Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.com/docs/how-to/querying-data/use-static-query/
- */
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+import { useStaticQuery, graphql } from "gatsby";
 
-import * as React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+const Seo = ({ seo = {} }) => {
+  const { allMarkdownRemark } = useStaticQuery(query);
+  const defaultSeo = allMarkdownRemark.nodes[0].frontmatter
 
-const Seo = ({ description, title, children }) => {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            social {
-              twitter
-            }
-          }
+  // Merge default and page-specific SEO values
+  const fullSeo = { ...defaultSeo, ...seo };
+
+  const getMetaTags = () => {
+    const tags = [];
+
+    if (fullSeo.title) {
+      tags.push(
+        {
+          property: "og:title",
+          content: fullSeo.title,
+        },
+        {
+          name: "twitter:title",
+          content: fullSeo.title,
         }
-      }
-    `
-  )
+      );
+    }
+    if (fullSeo.description) {
+      tags.push(
+        {
+          name: "description",
+          content: fullSeo.description,
+        },
+        {
+          property: "og:description",
+          content: fullSeo.description,
+        },
+        {
+          name: "twitter:description",
+          content: fullSeo.description,
+        }
+      );
+    }
+    if (fullSeo.keywords) {
+      tags.push(
+        {
+          name: "keywords",
+          content: fullSeo.keywords,
+        }
+      )
+    }
+    if (fullSeo.shareImage) {
+      const imageUrl =
+        (process.env.GATSBY_ROOT_URL || "http://localhost:8000") +
+        fullSeo.shareImage.file.publicURL;
+      tags.push(
+        {
+          name: "image",
+          content: imageUrl,
+        },
+        {
+          property: "og:image",
+          content: imageUrl,
+        },
+        {
+          name: "twitter:image",
+          content: imageUrl,
+        }
+      );
+    }
+    if (fullSeo.article) {
+      tags.push({
+        property: "og:type",
+        content: "article",
+      });
+    }
+    tags.push({ name: "twitter:card", content: "summary_large_image" });
 
-  const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
+    return tags;
+  };
+
+  const metaTags = getMetaTags();
 
   return (
-    <>
-      <title>{defaultTitle ? `${title} | ${defaultTitle}` : title}</title>
-      <meta name="description" content={metaDescription} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={metaDescription} />
-      <meta property="og:type" content="website" />
-      <meta name="twitter:card" content="summary" />
-      <meta
-        name="twitter:creator"
-        content={site.siteMetadata?.social?.twitter || ``}
-      />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={metaDescription} />
-      {children}
-    </>
-  )
-}
+    <Helmet
+      title={fullSeo.title}
+      titleTemplate={`${fullSeo.siteName} – %s`}
+      link={[
+        {
+          rel: "icon",
+          href: fullSeo.favicon.publicURL,
+        }
+      ]}
+      script={[
+      ]}
+      meta={metaTags}
+    />
+  );
+};
 
-export default Seo
+export default Seo;
+
+Seo.propTypes = {
+  title: PropTypes.string,
+  description: PropTypes.string,
+  image: PropTypes.string,
+  article: PropTypes.bool,
+};
+
+Seo.defaultProps = {
+  title: null,
+  description: null,
+  image: null,
+  article: false,
+};
+
+const query = graphql`
+{
+  allMarkdownRemark(filter: {fileAbsolutePath: {regex: "/(seo)/"  }}) {
+    nodes {
+      frontmatter {
+        siteName
+        title
+        description
+        keywords
+        favicon {
+          publicURL
+        }
+      }
+    }
+  }
+}
+`;
